2b9d4bb2498e3f92cec6df2065193ed6601be21b
LaRaye Johnson <lnj2105@gmail.com>
2021-04-16 00:41:22 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,8 +1,18 @@
 class Pokemon
   attr_accessor :id, :name, :type, :db, :hp
+<<<<<<< HEAD
 
   def initialize (id:, name:, type:, db:, hp: nil)
     @id = id
+=======
+  
+<<<<<<< HEAD
+  def initialize (id:, name:, type:, db:, hp: nil)
+=======
+  def initialize (id:, name:, type:, db:, hp: 60)
+>>>>>>> c1ebca52eeed757b38c28cba562af67c62147e03
+    @id = id 
+>>>>>>> 4e36e32281cbdee97482301fe205d34d3bf3a477
     @name = name
     @type = type
     @db = db
@@ -14,14 +24,27 @@ class Pokemon
   end
 
   def self.find(id, db)
+<<<<<<< HEAD
     pokemon = db.execute("SELECT * FROM pokemon WHERE pokemon.id = ?", id).flatten
     #binding.pry
     new_pokemon = Pokemon.new(id: id, name: pokemon[1], type: pokemon[2], db: db, hp: pokemon[3])
     #binding.pry
+<<<<<<< HEAD
   end
+=======
+=======
+    pokemon = db.execute("SELECT * FROM pokemon WHERE pokemon.id = ?", id).flatten!
+    new_pokemon = Pokemon.new(id: id, name: pokemon[1], type: pokemon[2], db: db)
+>>>>>>> c1ebca52eeed757b38c28cba562af67c62147e03
+  end 
+>>>>>>> 4e36e32281cbdee97482301fe205d34d3bf3a477
 
   def alter_hp(new_hp, db)
     db.execute("UPDATE pokemon SET hp = ? WHERE id = ?", new_hp, self.id)
+<<<<<<< HEAD
+=======
+    self.hp = new_hp
+>>>>>>> c1ebca52eeed757b38c28cba562af67c62147e03
     #binding.pry
   end
 end
